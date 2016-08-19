f65456b191a67d05fccb59e27b91c7771031a04f
ShengHao <641776671@qq.com>
2016-08-19 13:02:50 +0800

    建立testModel

---
+++ MyOffice/testModel.h
@@ -0,0 +1,13 @@
+//
+//  testModel.h
+//  MyOffice
+//
+//  Created by mac on 16/8/19.
+//  Copyright © 2016年 Unique. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testModel : NSObject
+
+@end
